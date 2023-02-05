827f35df219da4183197f88ef6f7739a10927f4d
RssMM <ymahboub@jaumebalmes.net>
2023-02-05 23:28:08 +0100

    first commit

--- src/main/java/com/mycompany/m06_uf2_act2/Modificar.java
+++ src/main/java/com/mycompany/m06_uf2_act2/Modificar.java
@@ -17,8 +17,8 @@ public class Modificar {
     
     public static void main(String[] args) {
         
-         //getFilmsInterval();
-        //getFilmsByDirector();
+        getFilmsInterval();
+        getFilmsByDirector();
         insertNewFilm();
     }
     
